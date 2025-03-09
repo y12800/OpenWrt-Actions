a7f2d37628da4d14904e37a5322d65f3aef74df4
y12800 <115250636+y12800@users.noreply.github.com>
2025-03-09 20:50:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -66,17 +66,10 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' packag
 sed -i '$a\admin:x:0:0:root:/root:/bin/ash' package/base-files/files/etc/passwd
 sed -i '$a\admin:$5$KGP.38nocVHkeNPa$9gNnoYzKRS2oeHzUn4UFZZ5wGAqmDNPZ04sPBASRpP/:0:0:99999:7:::' package/base-files/files/etc/shadow
 sed -i '$a\config login\n\toption username '\''admin'\''\n\toption password '\''$p$admin'\''\n\tlist read '\''*'\''\n\tlist write '\''*'\''' package/system/rpcd/files/rpcd.config
-sed -i 's/option rfc1918_filter 1/option rfc1918_filter 0/' package/system/uhttpd/files/uhttpd.config
-sed -i '/list listen_http\t[::]:80/a\    list listen_http    0.0.0.0:88\n    list listen_http    [::]:88' package/system/uhttpd/files/uhttpd.config
-sed -i '/list listen_https[[:space:]]/s/443/4443/' package/system/uhttpd/files/uhttpd.config
+sed -i '/\[::\]:80/a\\tlist listen_http\t0.0.0.0:88\n\tlist listen_http\t[::]:88' package/network/services/uhttpd/files/uhttpd.config
+sed -i 's/option rfc1918_filter 1/option rfc1918_filter 0/' package/network/services/uhttpd/files/uhttpd.config
+sed -i '/list listen_https[[:space:]]/s/443/4443/' package/network/services/uhttpd/files/uhttpd.config
 sed -i 's/REJECT/ACCEPT/1;s/REJECT/ACCEPT/1;s/REJECT/ACCEPT/1;s/REJECT/ACCEPT/1' package/network/config/firewall/files/firewall.config
-
-
-# sed -i '/list[[:space:]]\+listen_http[[:space:]]\+
-
-# \[::\]
-
-# :80/a\  list listen_http    0.0.0.0:88\n\tlist listen_http  [::]:88' package/system/uhttpd/files/uhttpd.config
 
 
 
