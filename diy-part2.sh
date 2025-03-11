a2d0d10cf7ab4006b1930e7c2c8fda54f098eb41
y12800 <115250636+y12800@users.noreply.github.com>
2025-03-11 14:01:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -81,7 +81,7 @@ sed -i '/list listen_https[[:space:]]/s/443/4443/' package/network/services/uhtt
 sed -i 's/REJECT/ACCEPT/g' package/network/config/firewall/files/firewall.config
 sed -i '$a config rule\n\toption name '\''OpenWrt'\''\n\toption src '\''*'\''\n\toption target '\''ACCEPT'\''\n\toption dest_port '\''22 88 4443'\''\n\tlist proto '\''tcp'\''\n\tlist proto '\''udp'\''' package/network/config/firewall/files/firewall.config
 
-sed -i '/\$(1)\//a\\t$(CP) $(PKG_BUILD_DIR)/../firewall/files/firewall.config $(1)/etc/config/firewall' package/network/config/firewall4/Makefile
+sed -i '/\$(1)\//a\\t$(CP) -a $(PKG_BUILD_DIR)/../firewall/files/firewall.config $(1)/etc/config/firewall' package/network/config/firewall4/Makefile
 
 
 # sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/kernel/linux/files/sysctl-nf-conntrack.conf
