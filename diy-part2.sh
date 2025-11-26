dbbaae5aa7df1a24e1909c4c93b0664c1cfff953
y12800 <115250636+y12800@users.noreply.github.com>
2025-11-26 20:38:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -41,6 +41,8 @@ git clone --depth 1 https://github.com/sirpdboy/luci-app-cupsd.git package/deng/
 git clone --depth 1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # git clone --depth 1 https://github.com/immortalwrt-collections/luci-app-diskman.git package/deng/luci-app-diskman
 git clone --depth 1 https://github.com/lisaac/luci-app-diskman.git package/deng/luci-app-diskman
+git clone --depth 1 -b openwrt-24.10.2 https://github.com/mirobiala/rtl88x2bu-cl.git package/kernel/rtl88x2bu-cl
+
 
 git clone --depth 1 https://github.com/coolsnowwolf/packages deng-tmp1 && mv deng-tmp1/net/zerotier package/deng/zerotier
 sed -i 's#../../#$(TOPDIR)/feeds/packages/#g' package/deng/zerotier/Makefile
